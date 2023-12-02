8d57548b02a60803187863973a5df1c358357ab4
hyunjialee <hyjialee@gmail.com>
2023-12-02 12:51:58 -0500

    added test for arraydeque

--- gettestie/src/test/java/rocks/zipcode/TestArrayDeque.java
+++ gettestie/src/test/java/rocks/zipcode/TestArrayDeque.java
@@ -21,33 +21,77 @@ public class TestArrayDeque {
 
     @Test
     public void testRemove(){
+        ArrayDeque<String> array = new ArrayDeque<>();
+        int expected = 1;
+        array.add("animal");
+        array.add("cat");
+        array.remove("animal");
+
+        int actual = array.size();
+        Assert.assertEquals(expected, actual);
+
 
     }
 
     @Test
     public void testClear(){
+        ArrayDeque<String> array = new ArrayDeque<>();
+        int expected = 0;
+        array.add("animal");
+        array.clear();
 
+        Assert.assertEquals(expected, array.size());
     }
 
 
     @Test
     public void testContains(){
+        ArrayDeque<String> array = new ArrayDeque<>();
+        String expected = "dog";
 
+        array.add("dog");
+
+        Assert.assertTrue(array.contains("dog"));
     }
 
     @Test
     public void testPeek(){
+        ArrayDeque<String> array = new ArrayDeque<>();
 
+        array.add("dog");
+        array.add("cat");
+        array.add("fish");
+
+        String expected = "dog";
+        Assert.assertEquals(expected, array.peek());
     }
 
     @Test
     public void testPoll(){
+        ArrayDeque<String> array = new ArrayDeque<>();
+
+        array.add("dog");
+        array.add("cat");
+        array.add("fish");
+
+        String expected = "cat";
+        array.poll();
+        Assert.assertEquals(expected, array.peek());
 
     }
-
     @Test
     public void testPop(){
+        ArrayDeque<String> array = new ArrayDeque<>();
 
+        array.add("dog");
+        array.add("cat");
+        array.add("fish");
+
+        String expected = "fish";
+
+        array.pop();
+        array.pop();
+
+        Assert.assertTrue(array.contains(expected));
     }
-    //pop, peek, add, remove
 }
