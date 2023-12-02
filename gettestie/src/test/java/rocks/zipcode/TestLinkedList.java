bfa395dafdaa53970190c84ef7efed5e95be8c77
hyunjialee <hyjialee@gmail.com>
2023-12-02 18:24:14 -0500

    finished

--- gettestie/src/test/java/rocks/zipcode/TestLinkedList.java
+++ gettestie/src/test/java/rocks/zipcode/TestLinkedList.java
@@ -1,11 +1,35 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class TestLinkedList {
 
+    LinkedList<Integer> linkedList;
+
+    @Before
+    public void setUp(){
+
+        linkedList = new LinkedList<>();
+        linkedList.add(1);
+        linkedList.add(2);
+        linkedList.add(3);
+    }
+
     @Test
     public void testAddAll(){
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(4);
+
+        linkedList.addAll(numbers);
+
+        Assert.assertEquals(4, linkedList.size());
 
         // getFirst, getLast
     }
@@ -13,10 +37,19 @@ public class TestLinkedList {
     @Test
     public void testFirst(){
 
+        Integer expected = 1;
+        Integer actual = linkedList.getFirst();
+        Assert.assertEquals(expected, actual);
+
     }
 
     @Test
     public void testGetLast(){
+
+        Integer expected = 3;
+        Integer actual = linkedList.getLast();
+
+        Assert.assertEquals(expected, actual);
 
     }
 }
